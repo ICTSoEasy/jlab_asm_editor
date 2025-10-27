71d1b684a006683dbd4775ed1dcb9c344a2ebc98
Oli Howson <info@ictsoeasy.co.uk>
2025-10-27 21:11:13 +0000

    “build”

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
-import {
-  JupyterFrontEnd,
-  JupyterFrontEndPlugin
-} from '@jupyterlab/application';
+import { JupyterFrontEnd, JupyterFrontEndPlugin } from '@jupyterlab/application';
+import { ILauncher } from '@jupyterlab/launcher';
+import { ICommandPalette } from '@jupyterlab/apputils';
+import { TextModelFactory } from '@jupyterlab/docregistry';
+
 
 /**
  * Initialization data for the jlab_asm_editor extension.
@@ -10,8 +11,33 @@ const plugin: JupyterFrontEndPlugin<void> = {
   id: 'jlab_asm_editor:plugin',
   description: 'A plugin to add an assembly editor to the launcher in tljh',
   autoStart: true,
-  activate: (app: JupyterFrontEnd) => {
+  requires: [ILauncher, ICommandPalette], // Ensure dependencies are listed
+  activate: (app: JupyterFrontEnd, launcher: ILauncher, palette: ICommandPalette) => {
     console.log('JupyterLab extension jlab_asm_editor is activated!');
+    const new_asm_file = 'jlab_asm_editor:create-asm';
+
+    app.commands.addCommand(new_asm_file, {
+      label: 'ASM File',
+      caption: 'Create a new ASM file',
+      iconClass: 'jp-FileIcon',
+      execute: () => {
+        const model = new TextModelFactory().createNew();
+        model.sharedModel.setSource('');
+        const widget = app.serviceManager.contents.newUntitled({
+          type: 'file',
+          ext: '.asm',
+        });
+        widget.then(file => {
+          //app.commands.execute('docmanager:open', { path: file.path });
+          app.commands.execute('docmanager:open', { path: file.path, factory: 'Editor' });
+        });
+      }
+    });
+
+    const category = 'File Operations';
+    launcher.add({ command: new_asm_file });
+    palette.addItem({ command: new_asm_file, category });
+
   }
 };
 
